626c39b46f8c2024d39fd1775ba41d72a9896bd0
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-10-08 23:09:05 +0530

    Add files via upload

---
+++ swap_case_hr.py
@@ -0,0 +1,39 @@
+"""
+Swap Case in Python HackerRank
+"""
+
+'''method 1'''
+
+# s=input()
+# a=s.swapcase()
+# print(a)
+
+
+'''method 2'''
+
+# print(input().swapcase())
+
+
+'''method 3'''
+
+# print('Chandu'.swapcase())
+
+
+'''method 4'''
+
+# def swap_case(s):
+#     return s.swapcase()
+# s=input()
+# a=s.swapcase()
+# print(a)
+
+
+'''method 5'''
+
+# def swap_case(s):
+#     return s.swapcase()
+# if __name__ == '__main__':
+#     s = input()
+#     result = swap_case(s)
+#     print(result)
+
